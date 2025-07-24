361b0ac9a203bfc946a873547e90dc0c78b8b092
shimoura <renanshimoura@gmail.com>
2025-07-24 10:08:58 +0200

    Add script to generate NEST target code from NESTML files

---
+++ testing-nestml-neurons/compile_nestml_neurons.py
@@ -0,0 +1,29 @@
+# Generate NEST target code from NESTML files
+
+from pynestml.frontend.pynestml_frontend import generate_nest_target
+from pathlib import Path
+
+nestml_file_path = (
+    Path(__file__).resolve().parent.parent
+    / "nestml-neurons/"
+)
+nestml_target_path = nestml_file_path / "nestml_target/"
+
+print(f"Generating NEST target code from {nestml_file_path} to {nestml_target_path}")
+
+generate_nest_target(
+    input_path=str(nestml_file_path),
+    target_path=str(nestml_target_path),
+    module_name="motor_neuron_module",
+)
+
+import nest
+from pathlib import Path
+
+# --- 1. Setup ---
+nest.ResetKernel()
+try:
+    nest.Install("motor_neuron_module")
+    print("NESTML target code generated and installed successfully.")
+except nest.NESTError:
+    exit()
