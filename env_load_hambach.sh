d2d36dda179bda31a8dae193fff56a3af2dcb596
shimoura <renanshimoura@gmail.com>
2025-08-08 16:51:03 +0200

    add LD_LIBRARY_PATH when loading env in the cluster so NESTML works

--- env_load_hambach.sh
+++ env_load_hambach.sh
@@ -1,2 +1,3 @@
 module load stable/25.07 ias6 gcc
 source "$(git rev-parse --show-toplevel)/venv/bin/activate"
+export LD_LIBRARY_PATH="/users/shimoura/gits/motor-controller-model/nestml-neurons/nestml_install:$LD_LIBRARY_PATH"
